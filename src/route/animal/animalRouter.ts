401de0465a58e9849f05823b81a9ecc7dc26ff85
Nouu <noelarrieulacoste@yahoo.fr>
2021-04-04 15:26:47 +0200

    implement animals routes

---
+++ src/route/animal/animalRouter.ts
@@ -0,0 +1,8 @@
+import express from "express";
+
+const animalRouter = express.Router();
+
+
+export {
+    animalRouter
+}
--- src/route/index.ts
+++ src/route/index.ts
@@ -1,8 +1,13 @@
 import {Express} from "express";
 import {enclosureRouter} from "./enclosure/enclosure.route";
 import {enclosureMaintenanceRouter} from "./enclosure/enclosure_maintenance.route";
+import {animalRouter} from "./animal/animalRouter";
+import {animalHealthBookRouter} from "./animal/animal_health_book.route";
 
 export function buildRoutes(app: Express) {
     app.use("/enclosure", enclosureRouter);
     app.use("/enclosure/maintenance", enclosureMaintenanceRouter);
+
+    app.use("/animal", animalRouter);
+    app.use("/animal/health", animalHealthBookRouter);
 }
