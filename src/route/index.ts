be366dcc8f222f7c5a039d2a49370dc92cbbf8eb
Nouu <noelarrieulacoste@yahoo.fr>
2021-04-04 15:32:57 +0200

    implements planning routers

--- src/route/index.ts
+++ src/route/index.ts
@@ -3,6 +3,8 @@ import {enclosureRouter} from "./enclosure/enclosure.router";
 import {enclosureMaintenanceRouter} from "./enclosure/enclosure_maintenance.router";
 import {animalRouter} from "./animal/animal.router";
 import {animalHealthBookRouter} from "./animal/animal_health_book.router";
+import {employeePlanningRouter} from "./planning/employee_planning.router";
+import {zooOpeningRouter} from "./planning/zoo_opening.router";
 
 export function buildRoutes(app: Express) {
     app.use("/enclosure", enclosureRouter);
@@ -10,4 +12,7 @@ export function buildRoutes(app: Express) {
 
     app.use("/animal", animalRouter);
     app.use("/animal/health", animalHealthBookRouter);
+
+    app.use("/planning", employeePlanningRouter);
+    app.use("/zoo", zooOpeningRouter);
 }
