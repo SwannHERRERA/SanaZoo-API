19b5ed20769f19bd337ed0c5b7cf3347542b943d
Nouu <noelarrieulacoste@yahoo.fr>
2021-04-04 15:21:05 +0200

    buildRoutes handle enclosure routes

--- src/route/index.ts
+++ src/route/index.ts
@@ -1,4 +1,8 @@
 import {Express} from "express";
+import {enclosureRouter} from "./enclosure/enclosure.route";
+import {enclosureMaintenanceRouter} from "./enclosure/enclosure_maintenance.route";
 
 export function buildRoutes(app: Express) {
+    app.use("/enclosure", enclosureRouter);
+    app.use("/enclosure/maintenance", enclosureMaintenanceRouter);
 }
