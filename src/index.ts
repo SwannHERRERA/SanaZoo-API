e4076f756e905c66591fa017543e4f070d2cbf83
Swann <swann@devloup.dev>
2021-04-05 10:11:17 +0200

    script should be like

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,15 @@
-import express, {Express} from "express";
-import {config} from "dotenv";
-import {SequelizeManager} from "./utils/db";
-import {buildRoutes} from "./route";
+import express from "express";
+import { config } from "dotenv";
+import { SequelizeManager } from "./utils/db";
+import { buildRoutes } from "./route";
 
 config();
 
-const app: Express = express();
+const app = express();
 
-/**
- * Parse body into json
- */
 app.use(express.json());
 
-/**
- * Build all routes
- */
-buildRoutes(app)
+buildRoutes(app);
 
 const sequelize = SequelizeManager.getInstance();
 
@@ -23,10 +17,10 @@ const sequelize = SequelizeManager.getInstance();
  * Just testing route, will be deleted in final release
  */
 app.get("/", (req: express.Request, res: express.Response) => {
-    res.send("Hello world !");
+  res.send("Hello world !");
 });
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 app.listen(port, function () {
-    console.log(`Listening on http://localhost:${port}`);
+  console.log(`Listening on http://localhost:${port}`);
 });
