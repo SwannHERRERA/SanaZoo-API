c04fa6b01072ba0b6dab1f08947be39166f1569a
Swann <swann@devloup.dev>
2021-04-05 09:35:22 +0200

    add swagger

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import express from "express";
 import { config } from "dotenv";
 import { SequelizeManager } from "./utils/db";
+import swaggerUI from "swagger-ui-express";
+import swaggerJSON from "../docs/swagger.json";
 config();
 
 const app = express();
 
 app.use(express.json());
-
-const sequelize = SequelizeManager.getInstance();
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerJSON));
+// const sequelize = SequelizeManager.getInstance();
 
 app.get("/", (req: express.Request, res: express.Response) => {
   res.send("Hello world !");
