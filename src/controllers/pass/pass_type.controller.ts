b45e3661eaaa3dc129f08e1ee1c304d514c47a36
Nouu <noelarrieulacoste@yahoo.fr>
2021-04-05 18:49:55 +0200

    pass type controller and get all route done

---
+++ src/controllers/pass/pass_type.controller.ts
@@ -0,0 +1,27 @@
+import {ModelCtor} from "sequelize";
+import {IPass_Instance, IPass_Type_Instance} from "../../models";
+import {SequelizeManager} from "../../utils/db";
+
+export class PassTypeController {
+    PassType: ModelCtor<IPass_Type_Instance>;
+    Pass: ModelCtor<IPass_Instance>;
+
+    private static instance: PassTypeController;
+
+    public static async getInstance(): Promise<PassTypeController> {
+        if (this.instance === undefined) {
+            const manager = await SequelizeManager.getInstance();
+            PassTypeController.instance = new PassTypeController(manager.PassType, manager.Pass);
+        }
+        return this.instance;
+    }
+
+    public async getAllPassTypes(): Promise<IPass_Type_Instance[]> {
+        return this.PassType.findAll();
+    }
+
+    constructor(PassType: ModelCtor<IPass_Type_Instance>, Pass: ModelCtor<IPass_Instance>) {
+        this.PassType = PassType;
+        this.Pass = Pass;
+    }
+}
--- src/route/pass/pass_type.router.ts
+++ src/route/pass/pass_type.router.ts
@@ -1,6 +1,32 @@
 import express from "express";
+import {PassTypeController} from "../../controllers/pass/pass_type.controller";
+import {authMiddleware} from "../../middlewares/auth.middleware";
 
 const passTypeRouter = express.Router();
+
+let passTypeController: PassTypeController;
+
+/**
+ * Find all pass type
+ */
+passTypeRouter.get('/', authMiddleware, async function (req, res, next) {
+    const result = await (await getPassTypeController()).getAllPassTypes();
+    console.log(result);
+    res.json(result);
+});
+
+/**
+ * Get pass type by id
+ */
+
+
+
+async function getPassTypeController(): Promise<PassTypeController> {
+    if (!passTypeController) {
+        passTypeController = await PassTypeController.getInstance();
+    }
+    return passTypeController;
+}
 
 export {
     passTypeRouter
