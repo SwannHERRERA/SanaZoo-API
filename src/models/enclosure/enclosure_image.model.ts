5883973ef7658e5431db98c39148f2439ffabca8
Nouu <noelarrieulacoste@yahoo.fr>
2021-04-05 11:38:49 +0200

    refacto models

--- src/models/enclosure/enclosure_image.model.ts
+++ src/models/enclosure/enclosure_image.model.ts
@@ -1,49 +1,49 @@
 import {
-  DataTypes,
-  Model,
-  ModelCtor,
-  Optional,
-  Sequelize,
+    DataTypes,
+    Model,
+    ModelCtor,
+    Optional,
+    Sequelize,
 } from "sequelize/types";
 
-export interface IEnclosure_image_Props {
-  id: number;
-  title: string;
-  path: string;
+export interface IEnclosure_Image_Props {
+    id: number;
+    title: string;
+    path: string;
 }
 
-export interface IEnclosure_image_creation_props
-  extends Optional<IEnclosure_image_Props, "id"> {}
+export interface IEnclosure_Image_Creation_Props extends Optional<IEnclosure_Image_Props, "id"> {
+}
 
-export interface Enclosure_image_instance
-  extends Model<IEnclosure_image_Props, IEnclosure_image_creation_props>,
-    IEnclosure_image_Props {}
+export interface IEnclosure_image_instance extends Model<IEnclosure_Image_Props, IEnclosure_Image_Creation_Props>,
+    IEnclosure_Image_Props {
+}
 
 const initEnclosureImage = (
-  sequelize: Sequelize
-): ModelCtor<Enclosure_image_instance> => {
-  return sequelize.define<Enclosure_image_instance>(
-    "Enclosure_Image",
-    {
-      id: {
-        type: DataTypes.BIGINT,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      title: {
-        type: DataTypes.STRING,
-      },
-      path: {
-        type: DataTypes.STRING,
-      },
-    },
-    {
-      freezeTableName: true,
-      underscored: true,
-      paranoid: true,
-      timestamps: true,
-    }
-  );
+    sequelize: Sequelize
+): ModelCtor<IEnclosure_image_instance> => {
+    return sequelize.define<IEnclosure_image_instance>(
+        "Enclosure_Image",
+        {
+            id: {
+                type: DataTypes.BIGINT,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            title: {
+                type: DataTypes.STRING,
+            },
+            path: {
+                type: DataTypes.STRING,
+            },
+        },
+        {
+            freezeTableName: true,
+            underscored: true,
+            paranoid: true,
+            timestamps: true,
+        }
+    );
 };
 
 export default initEnclosureImage;
--- src/models/enclosure/enclosure_service_book.model.ts
+++ src/models/enclosure/enclosure_service_book.model.ts
@@ -1,42 +1,42 @@
-import { Sequelize, Optional, Model, DataTypes, ModelCtor } from "sequelize";
+import {Sequelize, Optional, Model, DataTypes, ModelCtor} from "sequelize";
 
-export interface Enclosure_Service_Props {
-  id: number;
-  date: Date;
-  description: string;
+export interface IEnclosure_Service_Book_Props {
+    id: number;
+    date: Date;
+    description: string;
 }
 
-export interface Enclosure_Service_Creation_Props
-  extends Optional<Enclosure_Service_Props, "id"> {}
+export interface IEnclosure_Service_Book_Creation_Props extends Optional<IEnclosure_Service_Book_Props, "id"> {
+}
 
-export interface Enclosure_Service_Instance
-  extends Model<Enclosure_Service_Props, Enclosure_Service_Creation_Props>,
-    Enclosure_Service_Props {}
+export interface IEnclosure_Service_Book_Instance extends Model<IEnclosure_Service_Book_Props, IEnclosure_Service_Book_Creation_Props>,
+    IEnclosure_Service_Book_Props {
+}
 
 export default function (
-  sequelize: Sequelize
-): ModelCtor<Enclosure_Service_Instance> {
-  return sequelize.define<Enclosure_Service_Instance>(
-    "Enclosure_Service",
-    {
-      id: {
-        type: DataTypes.BIGINT,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      date: {
-        type: DataTypes.DATE,
-        unique: true,
-      },
-      description: {
-        type: DataTypes.TEXT,
-      },
-    },
-    {
-      freezeTableName: true,
-      underscored: true,
-      paranoid: true,
-      timestamps: true,
-    }
-  );
+    sequelize: Sequelize
+): ModelCtor<IEnclosure_Service_Book_Instance> {
+    return sequelize.define<IEnclosure_Service_Book_Instance>(
+        "Enclosure_Service",
+        {
+            id: {
+                type: DataTypes.BIGINT,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            date: {
+                type: DataTypes.DATE,
+                unique: true,
+            },
+            description: {
+                type: DataTypes.TEXT,
+            },
+        },
+        {
+            freezeTableName: true,
+            underscored: true,
+            paranoid: true,
+            timestamps: true,
+        }
+    );
 }
