b86d16ef6eda956dc755b949053f1d759cec1e6c
Clément BOSSARD <cbossard3@myges.fr>
2021-04-08 12:12:33 +0200

    Add enclosure CRUD

---
+++ src/controllers/enclosure/enclosure.controller.ts
@@ -0,0 +1,114 @@
+import {ModelCtor} from "sequelize";
+import {
+    IAnimal_Instance, IEnclosure_Creation_Props, IEnclosure_Image_Instance,
+    IEnclosure_Instance, IEnclosure_Props, IEnclosure_Service_Book_Instance,
+    IEnclosure_Type_Instance,
+    IEntry_Instance,
+    IPass_Enclosure_Access_Instance
+} from "../../models";
+import {SequelizeManager} from "../../utils/db";
+
+export interface IEnclosure_Get_All_Params {
+    offset: number;
+    limit: number;
+}
+
+export class Enclosure_Controller {
+    Enclosure: ModelCtor<IEnclosure_Instance>;
+    Entry: ModelCtor<IEntry_Instance>;
+    PassEnclosureAccess: ModelCtor<IPass_Enclosure_Access_Instance>;
+    EnclosureType: ModelCtor<IEnclosure_Type_Instance>;
+    EnclosureServiceBook: ModelCtor<IEnclosure_Service_Book_Instance>;
+    Animal: ModelCtor<IAnimal_Instance>;
+    EnclosureImage: ModelCtor<IEnclosure_Image_Instance>;
+
+    private static  instance: Enclosure_Controller;
+
+    public static async getInstance():Promise<Enclosure_Controller> {
+        if (Enclosure_Controller.instance === undefined) {
+            const {Enclosure, Entry, PassEnclosureAccess, EnclosureType,
+                EnclosureServiceBook, Animal, EnclosureImage} = await SequelizeManager.getInstance();
+            Enclosure_Controller.instance = new Enclosure_Controller(Enclosure, Entry, PassEnclosureAccess, EnclosureType,
+            EnclosureServiceBook, Animal, EnclosureImage);
+        }
+        return Enclosure_Controller.instance;
+    }
+
+    private constructor(Enclosure: ModelCtor<IEnclosure_Instance>,
+                        Entry: ModelCtor<IEntry_Instance>,
+                        PassEnclosureAccess: ModelCtor<IPass_Enclosure_Access_Instance>,
+                        EnclosureType: ModelCtor<IEnclosure_Type_Instance>,
+                        EnclosureServiceBook: ModelCtor<IEnclosure_Service_Book_Instance>,
+                        Animal: ModelCtor<IAnimal_Instance>,
+                        EnclosureImage: ModelCtor<IEnclosure_Image_Instance>) {
+        this.Enclosure = Enclosure;
+        this.Entry = Entry;
+        this.PassEnclosureAccess = PassEnclosureAccess;
+        this.EnclosureType = EnclosureType;
+        this.EnclosureServiceBook = EnclosureServiceBook;
+        this.Animal = Animal;
+        this.EnclosureImage = EnclosureImage;
+    }
+
+    public async add(props: IEnclosure_Creation_Props): Promise<IEnclosure_Instance | null> {
+        return this.Enclosure.create({
+            ...props
+        });
+    }
+
+    public async remove(id: number): Promise<number> {
+        return this.Enclosure.destroy({
+            where: {
+                id
+            }
+        });
+    }
+
+    public async update(id: number, props: IEnclosure_Creation_Props): Promise<IEnclosure_Instance | null> {
+        await this.Enclosure.update(props, {
+            where: {
+                id
+            }
+        });
+
+        return this.Enclosure.findOne({
+            where: {
+                id
+            }
+        });
+    }
+
+    public async getOneEnclosure(id: number): Promise<IEnclosure_Instance | null> {
+        return this.Enclosure.findOne({
+            where: {
+                id
+            }
+        });
+    }
+
+    public async getAllEnclosure(params? : IEnclosure_Get_All_Params ): Promise<IEnclosure_Instance[] | null> {
+        let limit = params?.limit || 20;
+        let offset = params?.offset || 0;
+
+        return this.Enclosure.findAll({
+            limit,
+            offset
+        });
+    }
+
+    public async getImages(id: number): Promise<IEnclosure_Image_Instance[] | null> {
+        return this.EnclosureImage.findAll({
+            where: {
+                enclosureId: id
+            }
+        });
+    }
+
+    public async getServiceBook(id: number): Promise<IEnclosure_Service_Book_Instance[] | null> {
+        return this.EnclosureServiceBook.findAll({
+            where: {
+                enclosureId: id
+            }
+        });
+    }
+}
--- src/route/enclosure/enclosure.router.ts
+++ src/route/enclosure/enclosure.router.ts
@@ -1,7 +1,131 @@
 import express from "express";
+import {adminMiddleware} from "../../middlewares/admin.middleware";
+import * as yup from "yup";
+import {Enclosure_Controller} from "../../controllers/enclosure/enclosure.controller";
 
 const enclosureRouter = express.Router();
 
+enclosureRouter.post("/", adminMiddleware,  async function(req, res) {
+    const name = req.body.name;
+    const capacity = req.body.capacity;
+    const description = req.body.description;
+    const visitDuration = req.body.visitDuration;
+    const handicapAccess = req.body.handicapAccess;
+    const maintenance = req.body.maintenance;
+    const enclosureTypeId = req.body.enclosureTypeId;
+    enclosureSchemaCreat.validate({
+            name,
+            capacity,
+            description,
+            visitDuration,
+            handicapAccess,
+            maintenance,
+            enclosureTypeId
+    }).then(async function() {
+        const controller = await Enclosure_Controller.getInstance();
+        const result = await controller.add({
+            name,
+            capacity,
+            description,
+            visitDuration,
+            handicapAccess,
+            maintenance,
+            enclosureTypeId
+        });
+        if (!result)
+            res.status(500).end();
+        res.status(201).json(result).end();
+    }).catch ((err) => {
+        res.status(400).json(err.message).end();
+    });
+})
+
+enclosureRouter.get("/", async function(req, res) {
+    const offset: number = req.body.offset;
+    const limit: number = req.body.limit;
+
+    const controller = await Enclosure_Controller.getInstance();
+    const result = await controller.getAllEnclosure({
+        limit,
+        offset
+    });
+
+    if (!result)
+        res.status(500).end();
+    res.status(200).json(result).end();
+});
+
+enclosureRouter.get("/:id", async function(req, res){
+    const controller = await Enclosure_Controller.getInstance();
+    const result = await controller.getOneEnclosure(Number.parseInt(req.params.id));
+    if (!result)
+        res.status(404).end();
+    res.status(200).json(result).end();
+})
+
+enclosureRouter.delete("/:id", adminMiddleware, async function(req, res){
+    const controller = await Enclosure_Controller.getInstance();
+    const number = await controller.remove(Number.parseInt(req.params.id));
+
+    if (number === 0)
+        res.status(404).end();
+    res.status(200).end();
+})
+
+enclosureRouter.put("/:id", adminMiddleware, async function (req, res) {
+    const name = req.body.name;
+    const capacity = req.body.capacity;
+    const description = req.body.description;
+    const visitDuration = req.body.visitDuration;
+    const handicapAccess = req.body.handicapAccess;
+    const maintenance = req.body.maintenance;
+    const enclosureTypeId = req.body.enclosureTypeId;
+    enclosureSchemaUpdate.validate({
+        name,
+        capacity,
+        description,
+        visitDuration,
+        handicapAccess,
+        maintenance,
+        enclosureTypeId
+    }).then(async function() {
+        const controller = await Enclosure_Controller.getInstance();
+        const result = await controller.update(Number.parseInt(req.params.id), {
+            name,
+            capacity,
+            description,
+            visitDuration,
+            handicapAccess,
+            maintenance,
+            enclosureTypeId
+        });
+        res.status(200).json(result).end();
+    }).catch((err) => {
+        res.status(500).json(err.message).end();
+    })
+});
+
+const enclosureSchemaUpdate = yup.object().shape({
+    name: yup.string().optional(),
+    capacity: yup.number().optional(),
+    description: yup.string().optional(),
+    visitDuration: yup.number().optional(),
+    handicapAccess: yup.boolean().optional(),
+    maintenance: yup.boolean().optional(),
+    enclosureTypeId: yup.number().optional()
+});
+
+const enclosureSchemaCreat = yup.object().shape({
+    name: yup.string().required(),
+    capacity: yup.number().required(),
+    description: yup.string().required(),
+    visitDuration: yup.number().required(),
+    handicapAccess: yup.boolean().required(),
+    maintenance: yup.boolean().required(),
+    enclosureTypeId: yup.number().required()
+});
+
 export {
+
     enclosureRouter
 }
