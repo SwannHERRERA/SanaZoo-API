8eeb32667c1bd4f64d32cbdb4f8a4f9d8bce61bf
Nouu <noelarrieulacoste@yahoo.fr>
2021-04-10 17:13:39 +0200

    escape game left

--- src/controllers/pass/pass_entry.controller.ts
+++ src/controllers/pass/pass_entry.controller.ts
@@ -2,15 +2,16 @@ import {ModelCtor} from "sequelize";
 import {
     IEnclosure_Instance,
     IEntry_Instance,
-    IPass_Creation_Props,
     IPass_Enclosure_Access_Instance,
     IPass_Instance,
-    IPass_Type_Instance,
-    IUser_Instance
+    IPass_Type_Instance
 } from "../../models";
 import {SequelizeManager} from "../../utils/db";
 import {Request, Response} from "express";
-import * as yup from "yup";
+
+enum PassType {
+    ESCAPE_GAME = 5
+}
 
 
 export class PassController {
@@ -67,6 +68,54 @@ export class PassController {
         const entries: IEntry_Instance[] = await this.Entry.findAll({where: {passId}});
 
         res.status(200).json(entries).end();
+    }
+
+    public async addEntry(req: Request, res: Response): Promise<void> {
+        const passId = req.body.passId;
+        const enclosureId = req.body.enclosureId;
+        /**
+         * Vérifier :
+         * Validité pass
+         * Pass enclosure access
+         * Si pass escape game, vérifier enclosure access ET l'ordre de passage
+         */
+        const pass: IPass_Instance | null = await this.Pass.findByPk(passId);
+        const enclosure: IEnclosure_Instance | null = await this.Enclosure.findByPk(enclosureId);
+        const passEnclosureAccessList: IPass_Enclosure_Access_Instance[] = await this.PassEnclosureAccess.findAll({where: {passId}});
+        const date: Date = new Date();
+
+        if (!pass || !enclosure || !passEnclosureAccessList.length) {
+            res.status(404).end();
+            return;
+
+        }
+
+        if (!passEnclosureAccessList.find((e) => {
+            return e.getEnclosure == enclosureId;
+        })) {
+            res.status(403).json("Your pass don't allow access to this enclosure").end();
+            return;
+        }
+
+        if (pass.validDate < date) {
+            res.status(403).json("Pass is expired").end();
+            return;
+
+        }
+
+        if (pass.passTypeId == PassType.ESCAPE_GAME) {
+            //TODO ESCAPE GAME
+            console.log('RESCAPE GAME');
+            await this.escapeGameEntry(res, pass, enclosure, passEnclosureAccessList);
+            return;
+        }
+
+        const entry: IEntry_Instance = await this.Entry.create({passId, enclosureId});
+        res.status(200).json(entry).end();
+    }
+
+    public async escapeGameEntry(res: Response, pass: IPass_Instance, enclosure: IEnclosure_Instance, passEnclosureAccessList: IPass_Enclosure_Access_Instance[]) {
+        
     }
 
     constructor(Enclosure: ModelCtor<IEnclosure_Instance>, PassType: ModelCtor<IPass_Type_Instance>, Pass: ModelCtor<IPass_Instance>, Entry: ModelCtor<IEntry_Instance>, PassEnclosureAccess: ModelCtor<IPass_Enclosure_Access_Instance>) {
