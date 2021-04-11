d61d851daf5e9b4c26b5451f2a3e2cfcf1f39d2d
Nouu <noelarrieulacoste@yahoo.fr>
2021-04-11 16:38:24 +0200

    add all sort of get entry on router

--- src/routes/pass/pass_entry.router.ts
+++ src/routes/pass/pass_entry.router.ts
@@ -1,16 +1,45 @@
 import express from "express";
-import {PassController} from "../../controllers/pass/pass.controller";
 import {EntryController} from "../../controllers/pass/pass_entry.controller";
+import {employeeMiddleware} from "../../middlewares/employee.middleware";
+import {passRouter} from "./pass.router";
 
 const passControlRouter = express.Router();
 let entryController: EntryController;
 
-async function getPassController(): Promise<EntryController> {
+async function getEntryController(): Promise<EntryController> {
     if (!entryController) {
         entryController = await EntryController.getInstance();
     }
     return entryController;
 }
+
+/**
+ * Get all entries
+ */
+passRouter.get('/', employeeMiddleware, async (req, res) => {
+    await (await getEntryController()).getAllEntries(req, res);
+});
+
+/**
+ * Get all entries for user
+ */
+passRouter.get('/user/:id', employeeMiddleware, async (req, res) => {
+    await (await getEntryController()).getEntriesByUserId(req, res);
+});
+
+/**
+ * Get all entries for pass
+ */
+passRouter.get('/pass/:id', employeeMiddleware, async (req, res) => {
+    await (await getEntryController()).getPassEntries(req, res);
+});
+
+/**
+ * Get all entries for enclosure
+ */
+passRouter.get('/enclosure/:id', employeeMiddleware, async (req, res) => {
+    await (await getEntryController()).getEnclosureEntries(req, res);
+});
 
 
 export {
