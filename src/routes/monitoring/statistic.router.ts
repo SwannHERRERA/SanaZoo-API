bf8806cb38340fe254abbe54b1e1af24a26d979d
Nouu <noelarrieulacoste@yahoo.fr>
2021-04-17 13:58:36 +0200

    add all statistics routes

--- src/routes/monitoring/statistic.router.ts
+++ src/routes/monitoring/statistic.router.ts
@@ -13,9 +13,42 @@ async function getStatisticController(): Promise<StatisticController> {
     return statisticController
 }
 
-statisticRouter.get('/affluence/daily', authMiddleware, async function (req, res) {
-    await (await getStatisticController()).dailyAffluence(res);
+statisticRouter.get('/count/user', authMiddleware, async function (req, res) {
+    await (await getStatisticController()).userCount(res);
 });
+
+statisticRouter.get('/count/enclosure', authMiddleware, async function (req, res) {
+    await (await getStatisticController()).enclosureCount(res);
+});
+
+statisticRouter.get('/count/animal', authMiddleware, async function (req, res) {
+    await (await getStatisticController()).animalCount(res);
+});
+
+statisticRouter.get('/count/animal/:enclosureId', authMiddleware, async function (req, res) {
+    await (await getStatisticController()).enclosureAnimalCount(req, res);
+});
+
+statisticRouter.get('/count/pass/valid/types', authMiddleware, async function (req, res) {
+    await (await getStatisticController()).validPassStatsByType(res);
+});
+
+statisticRouter.get('/count/pass/valid', authMiddleware, async function (req, res) {
+    await (await getStatisticController()).validPassStats(res);
+});
+
+statisticRouter.get('/count/pass/expired', authMiddleware, async function (req, res) {
+    await (await getStatisticController()).expiredPassStats(res);
+});
+
+statisticRouter.get('/count/pass/types', authMiddleware, async function (req, res) {
+    await (await getStatisticController()).allPassStatsByType(res);
+});
+
+statisticRouter.get('/count/pass', authMiddleware, async function (req, res) {
+    await (await getStatisticController()).allPassStats(res);
+});
+
 
 export {
     statisticRouter
