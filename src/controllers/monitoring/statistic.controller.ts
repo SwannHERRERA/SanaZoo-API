2af5637150f13deb32848d1bd7bc0f08393006db
Nouu <noelarrieulacoste@yahoo.fr>
2021-04-17 13:16:59 +0200

    statistic controller done

--- src/controllers/monitoring/statistic.controller.ts
+++ src/controllers/monitoring/statistic.controller.ts
@@ -1,11 +1,20 @@
 import {SequelizeManager} from "../../utils/db";
 import {ModelCtor, Op, Sequelize} from "sequelize";
-import {IAnimal_Instance, IEnclosure_Instance, IEntry_Instance, IPass_Instance, IUser_Instance} from "../../models";
+import {
+    IAnimal_Instance,
+    IEnclosure_Instance,
+    IEntry_Instance,
+    IPass_Instance,
+    IPass_Type_Instance,
+    IUser_Instance
+} from "../../models";
 import {Request, Response} from "express";
-import moment from "moment";
+import * as dateFns from 'date-fns';
+
 
 export class StatisticController {
     Pass: ModelCtor<IPass_Instance>;
+    PassType: ModelCtor<IPass_Type_Instance>;
     User: ModelCtor<IUser_Instance>;
     Entry: ModelCtor<IEntry_Instance>;
     Animal: ModelCtor<IAnimal_Instance>;
@@ -16,7 +25,7 @@ export class StatisticController {
     public static async getInstance(): Promise<StatisticController> {
         if (StatisticController.instance === undefined) {
             const manager = await SequelizeManager.getInstance();
-            StatisticController.instance = new StatisticController(manager.Pass, manager.User, manager.Entry, manager.Animal, manager.Enclosure);
+            StatisticController.instance = new StatisticController(manager.Pass, manager.User, manager.Entry, manager.Animal, manager.Enclosure, manager.PassType);
         }
         return StatisticController.instance;
     }
@@ -52,23 +61,22 @@ export class StatisticController {
     }
 
     public async validPassStats(res: Response): Promise<void> {
-        const today = moment().startOf('day');
-        const count = this.Pass.findAll({where: Sequelize.where(Sequelize.col('valid_date'), '>=', today)});
+        const today = dateFns.startOfToday();
+        const count = await this.Pass.findAll({where: Sequelize.where(Sequelize.col('valid_date'), '>=', today.toISOString())});
         res.status(200).json({
             'validPass': count
         });
     }
 
     public async expiredPassStats(res: Response): Promise<void> {
-        const today = moment().startOf('day');
-        const count = this.Pass.findAll({where: Sequelize.where(Sequelize.col('valid_date'), '<', today)});
+        const yesterday = dateFns.endOfYesterday()
+        const count = await this.Pass.findAll({where: Sequelize.where(Sequelize.col('valid_date'), '<=', yesterday.toISOString())});
         res.status(200).json({
             'expiredPass': count
         });
     }
 
     public async allPassStats(res: Response): Promise<void> {
-        const today = moment().startOf('day');
         const count = this.Pass.findAll();
         res.status(200).json({
             'totalPass': count
@@ -77,16 +85,45 @@ export class StatisticController {
     }
 
     public async validPassStatsByType(req: Request, res: Response): Promise<void> {
-
+        const today = dateFns.startOfToday();
+        const passTypeList: IPass_Type_Instance[] = await this.PassType.findAll();
+        const result: { passType: string, count: number; }[] = [];
+        for (const passType of passTypeList) {
+            const count: number = await this.Pass.count({
+                where: {
+                    [Op.and]: [
+                        Sequelize.where(Sequelize.col('valid_date'), '>=', today.toISOString()),
+                        {passTypeId: passType.id}
+                    ]
+                }
+            });
+            result.push({
+                passType: passType.name,
+                count
+            });
+        }
+        res.status(200).json(result);
     }
 
     public async allPassStatsByType(req: Request, res: Response): Promise<void> {
-
+        const passTypeList: IPass_Type_Instance[] = await this.PassType.findAll();
+        const result: { passType: string, count: number; }[] = [];
+        for (const passType of passTypeList) {
+            const count: number = await this.Pass.count({
+                where: {passTypeId: passType.id}
+            });
+            result.push({
+                passType: passType.name,
+                count
+            });
+        }
+        res.status(200).json(result);
     }
 
 
-    constructor(Pass: ModelCtor<IPass_Instance>, User: ModelCtor<IUser_Instance>, Entry: ModelCtor<IEntry_Instance>, Animal: ModelCtor<IAnimal_Instance>, Enclosure: ModelCtor<IEnclosure_Instance>) {
+    constructor(Pass: ModelCtor<IPass_Instance>, User: ModelCtor<IUser_Instance>, Entry: ModelCtor<IEntry_Instance>, Animal: ModelCtor<IAnimal_Instance>, Enclosure: ModelCtor<IEnclosure_Instance>, PassType: ModelCtor<IPass_Type_Instance>) {
         this.Pass = Pass;
+        this.PassType = PassType;
         this.User = User;
         this.Entry = Entry;
         this.Animal = Animal;
