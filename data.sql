894a1b6512d5db53295520a2834ffd00988e7101
Swann <swann@devloup.dev>
2021-04-18 17:04:30 +0200

    first jet on middleware

--- data.sql
+++ data.sql
@@ -10,3 +10,11 @@ insert into Pass_Type (id, name, price, created_at, updated_at)
 VALUES (5, 'PASS Escape game', 30, '2021-04-05', '2021-04-05');
 insert into Pass_Type (id, name, price, created_at, updated_at)
 VALUES (6, 'PASS Night', 40, '2021-04-05', '2021-04-05');
+insert into User_Role (id, name, created_at, updated_at)
+VALUES 
+  (1, 'ADMIN', '2021-04-18', '2021-04-18'),
+  (2, "EMPLOYEE", '2021-04-18', '2021-04-18'),
+  (3, "VETERINARY", '2021-04-18', '2021-04-18'),
+  (4, 'CLIENT', '2021-04-18', '2021-04-18');
+insert into User (first_name, last_name, email, birthdate, password, user_role_id, created_at, updated_at)
+VALUES ('Swann', 'HERRERA', 'swann@graines-octets.com', '2000-12-20 13:21:00', "", 1, '2021-04-18', '2021-04-18');
--- src/middlewares/employee.middleware.ts
+++ src/middlewares/employee.middleware.ts
@@ -1,4 +1,6 @@
 import { NextFunction, Request, Response } from "express";
+import { SequelizeManager } from "../utils/db";
+import { StatusCode } from "../utils/statusCode";
 import { authMiddleware } from "./auth.middleware";
 
 export async function employeeMiddleware(
@@ -7,6 +9,19 @@ export async function employeeMiddleware(
   next: NextFunction
 ): Promise<void> {
   await authMiddleware(req, res, async () => {
+    const EMPLOYEE_ROLES = ["ADMIN", "EMPLOYEE", "VETERINARY"];
+    const user = res.locals.user;
+    const { UserRole } = await SequelizeManager.getInstance();
+    const userRole = await UserRole.findByPk(user.userRoleId);
+    if (userRole === null) {
+      res.status(StatusCode.SERVER_ERROR).end();
+      console.error("User Role didn't exist");
+      return;
+    }
+    if (EMPLOYEE_ROLES.includes(userRole.name) === false) {
+      res.status(StatusCode.FORBIDDEN).end();
+      return;
+    }
     console.info(`Employee middleware from : ${req.url}`);
     next();
   });
