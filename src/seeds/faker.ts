786b658adc2fe3cb3f4f8ca5aa4ccb7fd830d91f
Swann <swann@devloup.dev>
2021-04-24 22:22:34 +0200

    fake animals and specie + healthBook

--- src/seeds/faker.ts
+++ src/seeds/faker.ts
@@ -6,9 +6,16 @@ import { Op } from "sequelize";
 export async function initFaker(): Promise<void> {
   // await createUser();
   // await createEncosureType();
-  // await createEncosures();
+  // await createEnclosure();
   // await createEnclosureImage();
   // await createEnclosureServiceBook();
+  // await createSpecies();
+  // await createAnimals();
+  // await createAnimalHealthBook();
+}
+
+function nextInt(max: number): number {
+  return Math.floor(Math.random() * Math.floor(max));
 }
 
 async function createUser(): Promise<void> {
@@ -72,8 +79,11 @@ async function createEncosureType() {
   }
 }
 
-async function createEncosures() {
+async function createEnclosure() {
+  const { Enclosure, EnclosureType } = await SequelizeManager.getInstance();
+  const enclosureTypes = await EnclosureType.findAll();
   for (let i = 0; i < 30; i++) {
+    const enclosureType = enclosureTypes[nextInt(enclosureTypes.length - 1)];
     const enclosure = {
       name: faker.system.fileName(),
       description: faker.commerce.productDescription(),
@@ -81,44 +91,116 @@ async function createEncosures() {
       visitDuration: faker.datatype.number(),
       handicapAccess: true,
       maintenance: false,
-      enclosureTypeId: faker.datatype.number(5) + 1,
+      enclosureTypeId: enclosureType.id,
       openHour: "8:20",
       closeHour: "17:40",
     };
-    const { Enclosure } = await SequelizeManager.getInstance();
     await Enclosure.create(enclosure);
   }
 }
 
 async function createEnclosureImage() {
+  const { Enclosure, EnclosureImage } = await SequelizeManager.getInstance();
+  const enclosures = await Enclosure.findAll();
+
   for (let i = 0; i < 100; i++) {
+    const enclosure = enclosures[nextInt(enclosures.length) - 1];
     const enclosureImage = {
       title: faker.lorem.word(),
       path: faker.image.animals(),
-      enclosureId: faker.datatype.number(30) + 1,
+      enclosureId: enclosure.id,
     };
-    const { EnclosureImage } = await SequelizeManager.getInstance();
     await EnclosureImage.create(enclosureImage);
   }
 }
 
 async function createEnclosureServiceBook() {
-  const { User, EnclosureServiceBook } = await SequelizeManager.getInstance();
+  const {
+    User,
+    EnclosureServiceBook,
+    Enclosure,
+  } = await SequelizeManager.getInstance();
   const employees = await User.findAndCountAll({
     where: { userRoleId: { [Op.ne]: 6 } },
   });
+  const enclosures = await Enclosure.findAndCountAll();
+
   for (let i = 0; i < 200; i++) {
     const employee = employees.rows[nextInt(employees.count - 1)];
+    const enclosure = enclosures.rows[nextInt(enclosures.count) - 1];
     const enclosureServiceBook = {
       date: faker.date.past(),
       description: faker.lorem.paragraph(),
-      enclosureId: faker.datatype.number(30) + 1,
+      enclosureId: enclosure.id,
       userId: employee.id,
     };
     await EnclosureServiceBook.create(enclosureServiceBook);
   }
 }
 
-function nextInt(max: number): number {
-  return Math.floor(Math.random() * Math.floor(max));
+async function createSpecies() {
+  const species = [
+    "dog",
+    "cat",
+    "snake",
+    "bear",
+    "lion",
+    "cetacean",
+    "horse",
+    "bird",
+    "cow",
+    "fish",
+    "crocodilia",
+    "insect",
+    "rabbit",
+    "type",
+  ];
+  const { Specie } = await SequelizeManager.getInstance();
+  species.forEach(async (name) => {
+    await Specie.create({
+      name,
+      description: faker.lorem.paragraph(),
+      origin: faker.lorem.word(),
+    });
+  });
+}
+
+async function createAnimals() {
+  const { Specie, Animal, Enclosure } = await SequelizeManager.getInstance();
+  const enclosures = await Enclosure.findAll();
+  const species = await Specie.findAll();
+  for (let i = 0; i < 200; i++) {
+    const specie = species[nextInt(species.length - 1)];
+    const enclosure = enclosures[nextInt(enclosures.length - 1)];
+    const animal = {
+      name: faker.name.findName(),
+      description: faker.lorem.paragraph(),
+      birthdate: faker.date.past().toISOString(),
+      image: faker.image.animals(),
+      enclosureId: enclosure.id,
+      specieId: specie.id,
+    };
+    await Animal.create(animal);
+  }
+}
+
+async function createAnimalHealthBook() {
+  const {
+    Animal,
+    AnimalHealthBook,
+    User,
+  } = await SequelizeManager.getInstance();
+  const animals = await Animal.findAll();
+  const verterinaires = await User.findAll({ where: { userRoleId: 5 } });
+  for (let i = 0; i < 200; i++) {
+    const animal = animals[nextInt(animals.length - 1)];
+    const verterinaire = verterinaires[nextInt(verterinaires.length - 1)];
+    const healthBook = {
+      description: faker.lorem.paragraph(),
+      date: faker.date.past().toISOString(),
+      animalId: animal.id,
+      userId: verterinaire.id,
+    };
+    await AnimalHealthBook.create(healthBook);
+  }
 }
